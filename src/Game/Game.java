552c09ff719e99487c0a79b1d1ee30ae2e0502e6
TheDischarger <TheDischarger@TheThrowBack>
2014-03-08 17:08:42 -0500

    Comments

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,18 +6,24 @@ import org.lwjgl.LWJGLException;
  
 public class Game {
 	public static void main(String[] args) {
+		// Initialize Display and set Properties
 		try {
-			Display.setDisplayMode(new DisplayMode(640, 480));
-			Display.setTitle("A fresh display!");
+			Display.setDisplayMode(new DisplayMode(1280, 720));
+			Display.setTitle("Infinite Turmoil - Prealpha v0.0.1 - Codename X");
 			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			Display.destroy();
 			System.exit(1);
+			
+		// Checks for Close
 		}
 		while(!Display.isCloseRequested()) {
 			Display.update();
 			Display.sync(60);
+			
+			
+		// Checks for Close
 		}
 		Display.destroy();
 		System.exit(0);
