86ec7ef52ee2d1c528a9b4eedbfb157b7bb85bd5
TheDischarger <TheDischarger@TheThrowBack>
2014-03-08 17:06:09 -0500

    Base Code

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,8 +1,25 @@
 package Game;
 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.LWJGLException;
+ 
 public class Game {
-public static void Main(String args[]){
-	boolean marcus = true;
-	boolean Dev12 = true;
-}
+	public static void main(String[] args) {
+		try {
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("A fresh display!");
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			Display.destroy();
+			System.exit(1);
+		}
+		while(!Display.isCloseRequested()) {
+			Display.update();
+			Display.sync(60);
+		}
+		Display.destroy();
+		System.exit(0);
+	}
 }
